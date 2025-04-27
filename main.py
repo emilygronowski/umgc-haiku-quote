6a4fc88f8077157876023bf5d66ea2850e8862ae
Emily Gronowski <emilygronowski3@gmail.com>
2025-04-27 10:58:57 -0400

    added main py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Name: Emily Gronowski
+Problem: The purpose of this program is to display a Haiku quote with the Author's name first.
+Date: 05/21/2024
+Version: 3.12
+"""
+
+# Display authors name first.
+print('Steve Tabb')
+# Display the first line of the quote containing 5 syllables.
+print('reaching the night sky')
+# Display the second line of the quote containing 7 syllables.
+print('a symphony of wind song')
+# Display the final line of the quote containing 5 syllables.
+print('in the pine forest')
